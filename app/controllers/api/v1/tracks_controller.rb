6ffcb131c1da960d61af68032bff6ff893f1ab2a
Luis Lopez <luigibyte@gmail.com>
2016-10-28 23:02:57 -0300

    securize api with token

--- app/controllers/api/v1/tracks_controller.rb
+++ app/controllers/api/v1/tracks_controller.rb
@@ -2,18 +2,28 @@ module Api
   module V1
     class TracksController < ApplicationController
       def index
-        begin
-          result = Sektor.search search_params
+        token = token_param
+
+        if token == Rails.application.secrets.api_token
+          result = Sektor.search search_param
           render json: result, status: :ok
-        rescue ActionController::ParameterMissing => e
-          render json: {error: e}, status: :bad_request
+        else
+          render json: { error: 'invalid token' }, status: :bad_request
         end
       end
 
+      rescue_from ActionController::ParameterMissing do |e|
+        render json: { error: e }, status: :bad_request
+      end
+
       private
-      
-      def search_params
-        params.require(:q)
+
+      def token_param
+        params.require :token
+      end
+
+      def search_param
+        params.require :q
       end
     end
   end
--- test/controllers/tracks_controller_test.rb
+++ test/controllers/tracks_controller_test.rb
@@ -2,12 +2,17 @@ require 'test_helper'
 
 class TracksControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
-    get api_v1_tracks_url(q: "we are the champions")
+    get api_v1_tracks_url(q: "we are the champions", token: "1234")
     assert_response :success
   end
 
   test "should require param q" do
-    get api_v1_tracks_url
+    get api_v1_tracks_url(token: "1234")
+    assert_response :bad_request
+  end
+
+  test "should require param token" do
+    get api_v1_tracks_url(q: "term")
     assert_response :bad_request
   end
 end
